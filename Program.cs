808348e62f7d534c3a572bceea4e1d853b0e31d1
bodie101 <keysbw@g.toiohomai.ac.nz>
2017-08-01 14:54:12 +1200

    user input

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace week04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Start the program with Clear();
+        Console.Clear();
+
+            Console.WriteLine("What is your first name");
+            var firstName = Console.ReadLine();
+            
+
+            Console.WriteLine("What is your last name");
+            var lastName = Console.ReadLine();
+
+        
+            
+           Console.WriteLine(firstName + lastName);
+        
+        //End the program with blank line and instructions
+        Console.ResetColor();
+        Console.WriteLine();
+        Console.WriteLine("Press <Enter> to quit the program");
+        Console.ReadKey();
+
+
+            
+        }
+    }
+}
