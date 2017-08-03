8facdf648a5ed9e9590ee224b9eb3a9f84b042be
bodie101 <keysbw@g.toiohomai.ac.nz>
2017-08-03 15:57:29 +1200

    userinput

---
+++ task2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+
+       
+        {
+
+            //Start the program with Clear();
+            Console.Clear();
+            
+
+            Console.WriteLine("put in number");
+            var input1 = Console.ReadLine();
+
+            var number1 = 0;
+            var isNumber = int.TryParse(input1, out number1);
+
+            if(isNumber) {
+                int counter = 12;
+
+                for (int i = 0; i < counter; i++)
+                {
+                    var a = i + 1;
+                    Console.WriteLine($"{input1} * {a} = {number1 * a} ");
+            
+
+                }
+            }
+            else {
+                Console.WriteLine("please enter a number not word");
+            }
+
+
+
+        
+
+            
+            //End the program with blank line and instructions
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+        }
+    }
+}
